0021fc7c789a810c848c8ff0c7c3943ec25df759
Creaous <68230846+Creaous@users.noreply.github.com>
2021-04-13 23:34:41 +0930

    Site Test 1 | Only original files

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ROBLOX Decompiled
- A decomplied version of ROBLOX sites (No copyright infringement is intended, for takedown please email me)
+A decomplied version of ROBLOX sites (No copyright infringement is intended, for takedown please email me)
+It is very unlikely that a working version can be made from these files, as they most likely use Node servers.
+Also, ROBLOX if your reading this (very unlikely), please just email me and I will take this down if you wish.
+
+## Site Test 1
+A decomplied version of the ROBLOX Site Test 1 site is contained under the "sitetest1.robloxlabs.com".
+It contains:
+- Original and formatted (WIP) files.
+- CSS Files
+- JS Files
+- The Configuration File
+- Maintenance Page
+(The only pages are from when it was in maintenance mode, which at the time of typing this it still is)
---
+++ sitetest1.robloxlabs.com/js.rbxcdn.com/45d0e0301651e6c76fd1547f4fcfe0f5.js
@@ -0,0 +1,5 @@
+;// bundle: page___1fef796ba7d7afcefb1b410d251fbb63_m
+;// files: common/deviceMeta.js
+
+;// common/deviceMeta.js
+var Roblox=Roblox||{};Roblox.DeviceMeta=function(){var t=document.querySelector('meta[name="device-meta"]');if(t===null){console.debug("Error loading device information from meta tag - please check if meta tag is present");return}var n=t.dataset||{},i={android:"android",ios:"ios",xbox:"xbox",uwp:"uwp",amazon:"amazon",win32:"win32",universalapp:"universalApp",unknown:"unknown"},r={computer:"computer",tablet:"tablet",phone:"phone",console:"console"};return function(){return{deviceType:r[n.deviceType]||"",appType:i[n.appType]||"",isInApp:n.isInApp==="true",isDesktop:n.isDesktop==="true",isPhone:n.isPhone==="true",isTablet:n.isTablet==="true",isConsole:n.isConsole==="true",isAndroidApp:n.isAndroidApp==="true",isIosApp:n.isIosApp==="true",isUWPApp:n.isUwpApp==="true",isXboxApp:n.isXboxApp==="true",isAmazonApp:n.isAmazonApp==="true",isWin32App:n.isWin32App==="true",isStudio:n.isStudio==="true",isIosDevice:n.isIosDevice==="true",isAndroidDevice:n.isAndroidDevice==="true",isUniversalApp:n.isUniversalApp==="true"}}}();
